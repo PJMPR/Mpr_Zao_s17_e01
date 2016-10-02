cb754412f7e942ae1fb86dbf893e92bd3d9ce2d1
PJMPR <adam.urbanow@gmail.com>
2016-10-02 17:31:01 +0200

    Update App.java

--- src/main/java/helloproject/App.java
+++ src/main/java/helloproject/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! some changes" );
+        System.out.println( "Hello World! some changes test" );
     }
 }
