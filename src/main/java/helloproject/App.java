a35b5437f77e730f0715d3de77cf887b476bb11a
Adam <aurbanowicz@underground-solutions.com.pl>
2016-10-04 14:52:50 +0200

    repair merge conflicts

--- src/main/java/helloproject/App.java
+++ src/main/java/helloproject/App.java
@@ -4,10 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
         System.out.println( "Hello World! some changes zmiany" );
-=======
         System.out.println( "Hello World! some changes test" );
->>>>>>> refs/remotes/origin/master
     }
 }
